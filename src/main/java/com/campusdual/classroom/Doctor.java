56d8557a5cc14e965026a87de81d08364ae67caa
Jorge González Fernández <j.gonzalezf@udc.es>
2024-12-17 21:16:01 +0100

    Herencia de métodos

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -2,10 +2,21 @@ package com.campusdual.classroom;
 
 public class Doctor extends Person {
 
-	protected String specialization;
+    protected String specialization;
 
-	public Doctor(String name, String surname, String specialization) {
-		super(name, surname);
-		this.specialization = specialization;
-	}
+    public Doctor(String name, String surname, String specialization) {
+        super(name, surname);
+        this.specialization = specialization;
+    }
+
+    @Override
+    public void getDetails() {
+        super.getDetails();
+        System.out.println(", Specialization: " + specialization);
+		/*
+		StringBuilder sp = new StringBuilder(", Specialization: ");
+		sp.append(specialization);
+		System.out.println(sp);
+		*/
+    }
 }
--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -1,11 +1,22 @@
 package com.campusdual.classroom;
 
 public class Person {
-	protected String name;
-	protected String surname;
+    protected String name;
+    protected String surname;
 
-	public Person(String name, String surname) {
-		this.name = name;
-		this.surname = surname;
-	}
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void getDetails() {
+        System.out.print("Name: " + name + ", Surname: " + surname);
+        /*
+		StringBuilder details = new StringBuilder("Name: ");
+		details.append(name);
+		details.append(", Surname: ");
+		details.append(surname);
+		System.out.print(details);
+		*/
+    }
 }
